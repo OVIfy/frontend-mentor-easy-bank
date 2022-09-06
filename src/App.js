16c364f988d797525ee4f9cb11a66579c4ae8b8f
DESKTOP-13RQSSJ\HP <Ezechukwuchirimmadu@gmail.com>
2022-09-06 21:34:21 +0100

    second

--- src/App.js
+++ src/App.js
@@ -7,9 +7,15 @@ import Destination from './components/pages/Destination';
 import Crew from './components/pages/Crew';
 import Technology from './components/pages/Technology';
 
-
-import './scss/style.scss'
 import './scss/_atoms.scss'
+import './scss/style.scss'
+import destImg from './images/destination/background-destination-desktop.jpg'
+import crewImg from './images/crew/background-crew-desktop.jpg'
+import techImg from './images/technology/background-technology-desktop.jpg'
+import homeImg from './images/home/background-home-desktop.jpg'
+
+
+
 
 
 
@@ -21,10 +27,10 @@ function App() {
   }
 
   return (
-    <Background path={state == "0" && `/images/home/background-home-desktop.jpg` ||
-                      state == "1" && `/images/destination/background-destination-desktop.jpg` ||
-                      state == "2" && `/images/crew/background-crew-desktop.jpg` ||
-                      state == "3" && `/images/technology/background-technology-desktop.jpg`
+    <Background path={state == "0" && `${homeImg}` ||
+                      state == "1" && `${destImg}` ||
+                      state == "2" && `${crewImg}` ||
+                      state == "3" && `${techImg}`
                      }>
       <NavBar classes="my2" items={['00 HOME','01 DESTINATION','02 CREW' , '03 TECHNOLOGY']} getstate={passState}/>
       {state == "0" &&  <Home/>}
--- src/crew.js
+++ src/crew.js
@@ -1,9 +1,10 @@
+
 export default [
     {
       "rank":"COMMANDER",
       "name":"DOUGLAS HURLEY",
       "bio": "Douglas Gerald Hurley is an American engineer, former Marine Corps pilot and former NASA astronaut. He launched into space for the third time as commander of Crew Dragon Demo-2.",
-      "img": "images/crew/image-douglas-hurley.png",
+      "img": "./images/crew/image-douglas-hurley.png",
     },
     {
         "rank":"MISSION SPECIALIST",
